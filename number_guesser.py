5a682eff55bd50e1913fe67b734f60e3183f8430
Sistik0 <brybrylopez@gmail.com>
2024-02-27 13:43:48 -0600

    Implemented a guess counter and output messages for correct and incorrect answers

--- number_guesser.py
+++ number_guesser.py
@@ -22,4 +22,21 @@ else:
 #randint includes upper range defined by stop variable
 
 random_number = random.randint(0,top_of_range)
-print(random_number)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time")
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    else:
+        print("You got it wrong :(")
+
+print("You got it in", guesses, "guesses")
